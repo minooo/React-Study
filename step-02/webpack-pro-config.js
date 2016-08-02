a9c40ec6cdee741cbc62a9e23d7dce0eaf054ac9
minooo <552594996@qq.com>
2016-08-03 07:37:26 +0800

    1

--- step-02/webpack-pro-config.js
+++ step-02/webpack-pro-config.js
@@ -28,7 +28,7 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     // 输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它.
 
-    publicPath: './',
+    publicPath: '',
     // 模板、样式、脚本、图片等资源对应的server上的路径
 
     filename: 'bundle.js'
