0c30c16368908845ce9c2d2af066e7f244fc9723
minooo <552594996@qq.com>
2016-08-04 20:40:46 +0800

    btn样式优化

--- step-02/src/js/components/CssTest/index.js
+++ step-02/src/js/components/CssTest/index.js
@@ -7,34 +7,34 @@ class CssTest extends Component {
   render() {
     return (
       <div className="mb20">
-        <button styleName="btnTest" className="center-center">
-          <i className="i-shoucang40 main-color font-size-18 pr5" />button标签带图标
+        <button styleName="btnTest" className="center-center mb10">
+          <i className="i-shoucang40 main-color font-size-24 pr5" />button标签带图标
         </button>
-        <button styleName="btnTest" className="text-overflow-1 block">
+        <button styleName="btnTest" className="text-overflow-1 block mb10">
           button标签按钮
         </button>
-        <button href="http://www.baidu.com" styleName="btnTest" className="text-overflow-1 block">
+        <button href="http://www.baidu.com" styleName="btnTest" className="text-overflow-1 block mb20">
           button标签按钮测试字符长度很长很长很长很长很长很长很长很长很长
         </button>
 
-        <a href="#" styleName="btnTest" className="center-center block">
-          <i className="i-shoucang40 main-color font-size-18 pr5" />a标签带图标
+        <a href="#" styleName="btnTest" className="center-center block mb10">
+          <i className="i-shoucang40 main-color font-size-24 pr5" />a标签带图标
         </a>
-        <a href="#" styleName="btnTest" className="text-overflow-1 center-center">
+        <a href="#" styleName="btnTest" className="text-overflow-1 block mb10">
           a标签按钮
         </a>
-        <a href="#" styleName="btnTest" className="text-overflow-1 block center-center">
+        <a href="#" styleName="btnTest" className="text-overflow-1 block mb20">
           a标签按钮测试字符长度试字符长度试字符长度试字符长度试字符长度
         </a>
 
-        <div styleName="btnTest" className="text-overflow-1">
-          <i className="i-shoucang40 main-color font-size-18 pr5" />div标签带图标
+        <div styleName="btnTest" className="center-center mb10">
+          <i className="i-shoucang40 main-color font-size-24 pr5" />div标签带图标
         </div>
-        <div styleName="btnTest" className="text-overflow-1">
+        <div styleName="btnTest" className="text-overflow-1 mb10">
           div标签按钮
         </div>
         <div styleName="btnTest" className="text-overflow-1">
-          div标签按钮测试字符长度测试字符长度测试字符长度测试字符长度测试字符长度
+          div标签按钮测试字符长度测试字符长度测试字符长度测试字符长度测试字符长符长
         </div>
       </div>
     )
