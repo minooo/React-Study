46b9a57505efb55adc56204168a01c32fdc0d006
minooo <552594996@qq.com>
2016-08-12 07:56:27 +0800

    one

--- step-02/src/js/components/BigComponent/BigOne/index.js
+++ step-02/src/js/components/BigComponent/BigOne/index.js
@@ -6,6 +6,7 @@ const BigOne = () => {
     <div>
       <h2 className={"font-size-16 mt10 " + styles.title}>
         这是一张大图开发(该图片&gt;10k,图片不转代码)
+        sf
       </h2>
       <div className={styles.bigImg}></div>
     </div>
