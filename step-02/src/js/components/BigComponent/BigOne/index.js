ff781acf4b16051208d10b52671f6e7ab28dbadb
minooo <552594996@qq.com>
2016-08-11 23:26:28 +0800

    dev

--- step-02/src/js/components/BigComponent/BigOne/index.js
+++ step-02/src/js/components/BigComponent/BigOne/index.js
@@ -5,7 +5,7 @@ const BigOne = () => {
   return (
     <div>
       <h2 className={"font-size-16 mt10 " + styles.title}>
-        这是一张大图开发(该图片&gt;10k,图片不转代码)
+        这是一张大图开发！(该图片&gt;10k,图片不转代码)
       </h2>
       <div className={styles.bigImg}></div>
     </div>
