67b966d1051ed0e4270da9b39b1eaa61bfcba7bb
liang xiang hui <552594996@qq.com>
2016-08-12 09:42:31 +0800

    reset

--- step-02/src/js/components/BigComponent/BigOne/index.js
+++ step-02/src/js/components/BigComponent/BigOne/index.js
@@ -7,8 +7,6 @@ const BigOne = () => {
       <h2 className={"font-size-16 mt10 " + styles.title}>
         这是一张大图开发(该图片&gt;10k,图片不转代码)
       </h2>
-      <h3>dev</h3>
-      <h3>go on master</h3>
       <div className={styles.bigImg}></div>
     </div>
   )
--- step-03/src/js/components/CommonTitle/index.js
+++ step-03/src/js/components/CommonTitle/index.js
@@ -4,13 +4,6 @@ const CommonTitle = ({title}) => {
   return (
     <div>
       <h1 className="font-size-24">{title}</h1>
-      <h1>dev 开发1</h1>
-      <h1>dev 开发2</h1>
-      <h1>dev 开发3</h1>
-      <h1>dev 开发4 未完待续</h1>
-      <h1>dev 开发5 未完成先提交</h1>
-      <h1>master1</h1>
-      <h1>master2</h1>
       <hr/>
     </div>
   )
