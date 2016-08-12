ab3046cb5be40361e7e97794bebefbecc4b47e28
liang xiang hui <552594996@qq.com>
2016-08-12 09:23:23 +0800

    master

--- step-02/src/js/components/BigComponent/BigOne/index.js
+++ step-02/src/js/components/BigComponent/BigOne/index.js
@@ -8,7 +8,7 @@ const BigOne = () => {
         这是一张大图开发(该图片&gt;10k,图片不转代码)
       </h2>
       <h3>dev</h3>
-      
+      <h3>go on master</h3>
       <div className={styles.bigImg}></div>
     </div>
   )
