62acff23152a3aeb9249a56b62a30d9061997c26
liang xiang hui <552594996@qq.com>
2016-08-20 17:59:39 +0800

    home元素添加

--- step-03/src/js/containers/Home.js
+++ step-03/src/js/containers/Home.js
@@ -1,16 +1,31 @@
 import React,{ Component } from 'react'
-import { Nav } from '../components'
+import { Button, NavBar, SearchBar } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
-export default class Home extends Component {
+class Home extends Component {
+  state = {
+    dark: false
+  }
+  switchDark() {
+    this.setState({ dark: !this.state.dark });
+  }
   render() {
+    const { getFieldProps } = this.props.form;
+    console.log(this.props.form.getFieldsValue());
     return (
       <div className="pageAnimate">
-        <h1>Home</h1>
-        <div>
-          <h1>我是首页</h1>
-        </div>
+        <SearchBar
+          {...getFieldProps('SearchBar')}
+          placeholder="嗖的一声"
+          onSubmit={(value) => { console.log(`onSubmit: ${value}`); }}
+          onClear={() => { console.log('onClear'); }}
+          onCancel={() => { console.log('onCancel'); }}
+        />
       </div>
     )
   }
 }
 
+Home = createForm()(Home);
+
+export default Home;
