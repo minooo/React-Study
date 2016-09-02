33126d9c8ff8a0f02585f967523c5d08f6ffdae4
liang xiang hui <552594996@qq.com>
2016-09-02 18:06:03 +0800

    fastNav

--- step-03/src/js/containers/Home.js
+++ step-03/src/js/containers/Home.js
@@ -1,37 +1,79 @@
 import React,{ Component } from 'react'
 import {WhiteSpace, Carousel } from 'antd-mobile'
-import { Header, Nav } from '../components'
+import { Header, Nav, FastNav } from '../components'
 
 class Home extends Component {
   state = {
-    dark: false
+    dark: false,
+    fastNav: [
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      },
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      },
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      },
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      },
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      },
+      {
+        link: "https://www.github.com",
+        thumb: "http://temp.im/140x140",
+        title: "大牌抢购"
+      }
+    ]
   }
   
   render() {
     const settings = {
       dots: true,
       autoplay: 1,
+      autoplaySpeed: 3000,
       infinite: true,
       mode: 'banner',
       initialSlide: this.state.current,
       afterChange: this.slideTo,
+      lazyLoad: true
     }
+    const { fastNav } = this.state
+
     return (
       <div>
         <Header />
-        <WhiteSpace size="lg" />
-          <Carousel {...settings}>
+        <Carousel {...settings}>
             <div className="item">
-              <img src="http://temp.im/1242x520/4CD964/fff" alt=""/>
+              <img src="http://temp.im/1125x354/444/fff" alt=""/>
             </div>
             <div className="item">
-              <img src="http://temp.im/1242x520/4CD964/fff" alt=""/>
+              <img src="http://temp.im/1125x354/444/fff" alt=""/>
             </div>
             <div className="item">
-              <img src="http://temp.im/1242x520/4CD964/fff" alt=""/>
+              <img src="http://temp.im/1125x354/444/fff" alt=""/>
+            </div>
+            <div className="item">
+              <img src="http://temp.im/1125x354/444/fff" alt=""/>
             </div>
           </Carousel>
-        <WhiteSpace size="lg" />
+        <div className="bg-white hor pt30">
+          {fastNav.map((item, index) =>
+            <FastNav key={index} data={item}/>
+          )}
+        </div>
         <Nav />
       </div>
     )
