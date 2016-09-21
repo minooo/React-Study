99fef5cd7a1312ebcdb455a505c79eea60fa968e
minooo <552594996@qq.com>
2016-09-21 22:53:05 +0800

    some

--- step-03/src/js/containers/Shop/Detail.js
+++ step-03/src/js/containers/Shop/Detail.js
@@ -5,8 +5,8 @@ export default class Detail extends Component {
   render() {
     return (
       <div className="box">
-        <NavBar mode="light" leftContent={[<Icon key="0"  />]} onLeftClick={() => {history.go(-1)}}>好店详情</NavBar>
-        空落落的什么也没有 - -！
+        <NavBar  mode="light" leftContent={[<Icon key="0"  />]} onLeftClick={() => {history.go(-1)}}>好店详情</NavBar>
+        <div className="font-34">空落落的什么也没有 - -！</div>
       </div>
     )
   }
