6de95609a0d4527966ef52720fe3399f55bf7df6
liang xiang hui <552594996@qq.com>
2016-09-27 09:21:22 +0800

    redux 初步构建

--- step-04/src/js/actions/CounterActions.js
+++ step-04/src/js/actions/CounterActions.js
@@ -16,7 +16,7 @@ export function incrementIfOdd() {
   return (dispatch, getState) => {
     const { counter } = getState();
 
-    if (counter % 2 === 0) {
+    if (counter % 2 !== 0) {
       return;
     }
 
--- step-04/src/js/components/Counter/index.js
+++ step-04/src/js/components/Counter/index.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import { Button } from 'antd-mobile'
 
-export default class Counter extends React.Component {
-  incrementIfOdd = () => {
-    if (this.props.counter % 2 == 0) {
-      this.props.increment()
-    }
-  }
-
-  incrementAsync = () => {
-    setTimeout(() => this.props.increment(), 1000)
-  }
-
-  render() {
-    const { counter, increment, decrement } = this.props
-    return (
-      <p>
-        点击: {counter} 次.
-        {' '}
-        <Button onClick={increment}>+</Button>
-        {' '}
-        <Button onClick={decrement}>-</Button>
-        {' '}
-        <Button onClick={this.incrementIfOdd}>如果是偶数就是加1</Button>
-        {' '}
-        <Button onClick={this.incrementAsync}>点击后1秒后加1</Button>
-      </p>
-    )
-  }
+const Counter = ({counter, increment, decrement, incrementIfOdd, incrementAsync}) => {
+  return (
+    <p>
+      点击: {counter} 次.
+      {' '}
+      <Button onClick={increment}>+</Button>
+      {' '}
+      <Button onClick={decrement}>-</Button>
+      {' '}
+      <Button onClick={incrementIfOdd}>如果是偶数就是加1</Button>
+      {' '}
+      <Button onClick={incrementAsync}>点击后1秒后加1</Button>
+    </p>
+  )
 }
+
+export default  Counter
