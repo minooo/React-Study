76f3c1f21d241742e06a9f3309a89e50da6761ca
liang xiang hui <552594996@qq.com>
2016-09-30 18:01:22 +0800

    todos

--- step-04/src/js/actions/actionsTypes.js
+++ step-04/src/js/actions/actionsTypes.js
@@ -2,5 +2,19 @@
 * 这里定义所有的action类型
 * */
 
+// Counter
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+// Todos
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const MARK_TODO = 'MARK_TODO';
+export const MARK_ALL = 'MARK_ALL';
+export const CLEAR_MARKED = 'CLEAR_MARKED';
+
+// TodoFilters
+export const SHOW_ALL = 'show_all';
+export const SHOW_MARKED = 'show_marked';
+export const SHOW_UNMARKED = 'show_unmarked';
---
+++ step-04/src/js/components/Todos/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <h1>todos</h1>
+      </header>
+    );
+  }
+}
+
