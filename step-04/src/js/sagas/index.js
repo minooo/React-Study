1a711466775e9fda57284da5b33bc0b14c7877a0
liang xiang hui <552594996@qq.com>
2016-10-20 13:35:31 +0800

    opt

--- step-04/src/js/sagas/index.js
+++ step-04/src/js/sagas/index.js
@@ -1,5 +1,16 @@
-import { takeEvery, delay, takeLatest } from 'redux-saga'
-import { put, call, take, fork, select } from 'redux-saga/effects'
+import { takeEvery, delay, takeLatest, buffers } from 'redux-saga'
+import { put, call, take, fork, select, actionChannel } from 'redux-saga/effects'
+
+// delay 延迟执行
+// takeEvery 监控某个动作，如果该动作被触发，则执行传入的 saga，如果动作被多次触发，则依次执行
+// takeLatest 监控某个动作，如果该动作被出发，则执行传入的 saga，如果动作被多次触发，则只执行最新的动作
+// buffers 限制存入序列的动作数量  const requestChan = yield actionChannel('REQUEST', buffers.sliding(5))
+
+// put 调用动作，dispatch adtion
+// take 监控某个动作
+// call 异步调用，阻塞
+// fork 异步调用，非阻塞（分流）
+// actionChannel 依次处理每个被监控的动作
 
 import {
   INCREMENT_COUNTER,
