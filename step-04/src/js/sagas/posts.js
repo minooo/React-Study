80071ade70ff738a09b8934e0da41d2228fe991b
liang xiang hui <552594996@qq.com>
2016-10-29 16:32:22 +0800

    asynchronous

--- step-04/src/js/sagas/posts.js
+++ step-04/src/js/sagas/posts.js
@@ -1,3 +1,54 @@
-/**
- * Created by minooo on 2016/10/29.
- */
+import {
+  takeEvery,
+  delay,
+  takeLatest,
+  buffers,
+  channel,
+  eventChannel,
+  END
+} from 'redux-saga'
+import {
+  put,
+  call,
+  take,
+  fork,
+  select,
+  actionChannel,
+  cancel,
+  cancelled
+} from 'redux-saga/effects'
+
+import fetch from 'isomorphic-fetch'
+import {
+  REQUEST_POSTS,
+  RECEIVE_POSTS
+} from '../actions/actionsTypes'
+
+// 异步获取数据，开始！
+function fetchPostsApi() {
+  return fetch(`http://www.reddit.com/r/reactjs.json` )
+    .then(response => response.json() )
+    .then(json => json.data.children.map(child => child.data) )
+}
+
+/*function* fetchPosts() {
+ const posts = yield call(fetchPostsApi)
+ yield put({type: RECEIVE_POSTS, posts})
+ }*/
+
+/*为什么会出错误？？*/
+/*function* fetchPosts() {
+ const posts = yield call(fetchPostsApi)
+ yield put(actions.onReceivePosts(posts))
+ }*/
+
+function* fetchPosts() {
+  const posts = yield call(fetchPostsApi)
+  yield put({type: RECEIVE_POSTS, posts})
+}
+
+export function* watchPost() {
+  while( yield take(REQUEST_POSTS) ){
+    yield fork(fetchPosts)
+  }
+}
