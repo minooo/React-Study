4b4e7788f7d34d5b21fe8409fc220dec12786878
minooo <552594996@qq.com>
2016-11-08 23:17:15 +0800

    scroll

--- step-04/src/js/components/ScrollBox/index.js
+++ step-04/src/js/components/ScrollBox/index.js
@@ -2,17 +2,30 @@ import React,{Component} from 'react'
 import ReactIScroll from 'react-iscroll'
 import iScroll from 'iscroll/build/iscroll-probe'
 
-const ScrollBox = ({children, ...rest}) => {
-  return (
-    <div>
+/*
+ const ScrollBox = ({children, ...rest}) => {
+ return (
+ <ReactIScroll
+ iScroll={iScroll}
+ {...rest}
+ >
+ {children}
+ </ReactIScroll>
+ )
+ }
+
+ export default ScrollBox*/
+
+export default class ScrollBox extends Component {
+  render() {
+    const {children, ...rest} = this.props
+    return (
       <ReactIScroll
         iScroll={iScroll}
         {...rest}
       >
         {children}
       </ReactIScroll>
-    </div>
-  )
+    )
+  }
 }
-
-export default ScrollBox
--- step-04/src/js/containers/Coupon/Detail.js
+++ step-04/src/js/containers/Coupon/Detail.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 import { ScrollBox } from 'app/components'
 
+import ReactIScroll from 'react-iscroll'
+import iScroll from 'iscroll/build/iscroll-probe'
+
 const options = {
-  click:true,
+  click: true,
   scrollX: true
+}
+
+const optionsList = {
+  click: true,
+  scrollY: true,
+  scrollbars: true,
 }
 
 export default class Detail extends Component {
   state = {
+    focus: 0,
     date: [
+      {week:"周一", date: '1'},
+      {week:"周一", date: '2'},
+      {week:"周一", date: '3'},
+      {week:"周一", date: '4'},
+      {week:"周一", date: '5'},
+      {week:"周一", date: '6'},
       {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
-      {week:"周一", date: '7'},
+      {week:"周一", date: '8'},
+      {week:"周一", date: '9'},
+      {week:"周一", date: '10'},
+      {week:"周一", date: '11'},
     ]
   }
 
   onClick = (i) => {
-    console.log(i)
+    this.setState({focus: i})
+    /*this.refs.iScroll.scrollToElement(document.querySelector('#list8'))*/
+    this.refs.iScroll.withIScroll(function(iScroll) {
+      iScroll.scrollToElement(document.querySelector(`#list${i}`))
+    })
   }
   render() {
-    const { date } = this.state
+    const { focus, date } = this.state
+
+    let i = 0, len = 50, listOfLi = []
+
+    for(i; i < len; i++) {
+      listOfLi.push(<li id={"list" + i} key={i} className="ptb20 center-center border-bottom">Row {i+1}</li>)
+    }
+
     return (
       <div className="box">
         <ScrollBox
@@ -40,14 +62,17 @@ export default class Detail extends Component {
             overflow: 'hidden'
           }}
         >
-          <div className="ver-center ptb20 bg-white">
+          <div className="ver-center ptb20 bg-white scroll-wrap">
             {
               date.map((item, index) =>
                 <div key={index}  className="center-center-column plr20">
-                  <div className="font-32 mb20 color4">{item.week}</div>
+                  <div className="font-32 mb20 color4 mb10">{item.week}</div>
                   <div
-                    className="circle center-center font-34"
-                    style={{width:'0.8rem', height:'0.8rem', backgroundColor:'red'}}
+                    className={classnames({
+                      "circle center-center font-34":true,
+                      "bg-main color-white": index === focus
+                    })}
+                    style={{width:'0.8rem', height:'0.8rem'}}
                     value={index}
                     onClick={this.onClick.bind(null, index)}
                   >
@@ -58,6 +83,56 @@ export default class Detail extends Component {
             }
           </div>
         </ScrollBox>
+
+        <ReactIScroll
+          ref="iScroll"
+          iScroll={iScroll}
+          options={optionsList}
+          style={{
+            position: 'absolute',
+            zIndex:15,
+            top: '2.2rem',
+            bottom: 0,
+            left: 0,
+            width:'100%',
+            background:'#fff',
+            overflow: 'hidden'
+          }}
+        >
+          <ul>
+            {listOfLi}
+          </ul>
+        </ReactIScroll>
+
+        {/*<ScrollBox
+          ref="iScroll"
+          options={optionsList}
+          style={{
+            position: 'absolute',
+            zIndex:15,
+            top: '2.2rem',
+            bottom: 0,
+            left: 0,
+            width:'100%',
+            background:'#fff',
+            overflow: 'hidden'
+          }}
+        >
+          <div>
+            {
+              date.map((item, index) =>
+                <div
+                  key={index}
+                  id={"list" + item.date}
+                  className="mb20 color-white center-center font-36"
+                  style={{height:'2rem', background:'tomato',}}
+                >
+                  {item.week+item.date}
+                </div>
+              )
+            }
+          </div>
+        </ScrollBox>*/}
       </div>
     )
   }
