480fd3b5e6bc86a7d9f078f5c591e9d1b394ab85
minooo <552594996@qq.com>
2016-11-12 22:50:17 +0800

    dev

--- step-04/src/js/containers/User/index.js
+++ step-04/src/js/containers/User/index.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react'
 import { Nav, Post, ScrollBox } from 'app/components'
 import { bindActionCreators } from 'redux'
@@ -21,11 +22,11 @@ class User extends Component {
   }
 
   onScrollStart= (e) => {
-    /*e.y < e.maxScrollY + 80*/
-    /*let Y = e.y
+    /!*e.y < e.maxScrollY + 80*!/
+    /!*let Y = e.y
     if (Y > 100) {
       console.log('放手！实现了！！！')
-    }*/
+    }*!/
   }
 
   onScroll = (e) => {
@@ -81,6 +82,134 @@ class User extends Component {
             isRefreshDown={isRefreshDown}
           />
         </ScrollBox>
+        <Nav />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  state: state.posts
+})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(PostActions, dispatch)
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(User)*/
+
+
+import React, { Component } from 'react'
+import { Nav } from 'app/components'
+import ReactIScroll from 'react-iscroll'
+import iScroll from 'iscroll/build/iscroll-probe'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as PostActions from 'app/actions/PostActions'
+
+class User extends Component {
+  state = {
+    scrollReloadHeight:0,
+    pullReload:false,
+    go:false,
+    scrollOptions: {
+      click:true,
+      scrollY: true,
+      probeType:3
+    }
+  }
+  componentDidMount(){
+    console.log(this.refs.scrollReload.offsetHeight)
+    this.setState({scrollReloadHeight:this.refs.scrollReload.offsetHeight})
+  }
+
+  onScrollStart= (e) => {
+    /*e.y < e.maxScrollY + 80*/
+    /*let Y = e.y
+     if (Y > 100) {
+     console.log('放手！实现了！！！')
+     }*/
+  }
+
+  onScroll = (e) => {
+    let Y = e.y, scrollReloadHeight = this.state.scrollReloadHeight
+
+    if(Y>0) {this.setState({go:true})} else {this.setState({go:false})}
+
+    if(Y>=scrollReloadHeight) {
+      this.setState({pullReload:true})
+    }else if( 0< Y < scrollReloadHeight) {
+      this.setState({pullReload:false})
+    }
+
+
+  }
+  onTouchEnd = () => {
+    this.setState({scrollOptions:{...this.state.scrollOptions, startY:this.state.scrollReloadHeight}})
+  }
+
+  onFlick = () => {
+    console.log('说是弹动。。。')
+  }
+
+  _handleScrollRefresh = (iScrollInstance) => {
+
+      this.state.go && this.refs.iScroll.withIScroll((iScroll) => {
+        iScroll.scrollTo(0, this.state.scrollReloadHeight)
+      })
+
+  }
+
+  render() {
+    const { state, actions } = this.props
+    const { pullReload,scrollOptions } = this.state
+
+    let i = 0, len = 70, listOfLi = []
+
+    for(i; i < len; i++) {
+      listOfLi.push(<li ref={"list" + i} key={i} className="ptb20 center-center border-bottom">Row {i+1}</li>)
+    }
+
+    return (
+      <div className="box">
+        <ReactIScroll
+          iScroll={iScroll}
+          ref="iScroll"
+          options={scrollOptions}
+          style={{
+            position: 'absolute',
+            width:'100%',
+            maxWidth: '10rem',
+            top: 0,
+            bottom: '1rem',
+            background:'#f2f2f2',
+            overflow: 'hidden'
+          }}
+          onRefresh={this._handleScrollRefresh}
+          onScroll={this.onScroll}
+          onTouchEnd={this.onTouchEnd}
+          onScrollStart={this.onScrollStart}
+          onFlick={this.onFlick}
+
+        >
+          <div className="relative" style={{background:'tomato'}}>
+            <div
+              style={{
+                position: 'absolute',
+                width:'100%',
+                top: '-0.6rem',
+                height:'0.6rem',
+                background:!pullReload?'yellow':'blue'
+              }}
+              ref="scrollReload"
+              className="center-center"
+            >{!pullReload?'下拉刷新':"放手刷新"}</div>
+            {listOfLi}
+          </div>
+        </ReactIScroll>
         <Nav />
       </div>
     )
